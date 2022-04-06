f5567e32f237c6439b836111a7c0c031e40ee348
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-06 09:05:50 +0100

    add employee table

---
+++ EmployeeManager/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManager.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Notes { get; set; }
+        public string Country { get; set; }
+
+    }
+}
