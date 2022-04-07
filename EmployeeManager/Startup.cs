42c2442f4bf34d18dcfca66ec4514769c4c2df24
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-07 22:51:01 +0100

    add startup.cs

---
+++ EmployeeManager/Startup.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeManager.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(EmployeeManager.Startup))]
+namespace EmployeeManager
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AppDb")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                          name: "default",
+                          pattern: "{controller=EmployeeManager}/{action=List}/{id?}");
+            });
+        }
+    }
+}
