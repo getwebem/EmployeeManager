94bb15fc4a02a976a5342adf701a4fcbf2d30138
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-12 09:25:21 +0100

    add controller

---
+++ EmployeeManager/Controllers/EmployeeManagerController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManager.Controllers
+{
+    public class EmployeeManagerController : Controller
+    {
+        private AppDbContext db = null;
+        public EmployeeManagerController(AppDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult List()
+        {
+            List<Employee> model = (from e in db.Employees
+                                    orderby e.EmployeeID
+                                    select e).ToList();
+            return View(model);
+        }
+      
+    }
+}
