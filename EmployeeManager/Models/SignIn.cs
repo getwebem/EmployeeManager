6eded03dfc10334923ddb907ad11fdec01aca93c
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-15 00:17:25 +0100

    update signin page (migration error active)

--- EmployeeManager/Models/SignIn.cs
+++ EmployeeManager/Models/SignIn.cs
@@ -1,9 +1,19 @@
-﻿namespace EmployeeManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManager.Models
 {
     public class SignIn
     {
+        [Required]
+        [Display(Name ="User Name")]
         public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Remember Me")]
         public bool RememberMe { get; set; }
 
     }
