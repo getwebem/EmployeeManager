4f06dbee9c936018ab3cf99c7bff5c594b1f2dc4
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-15 00:50:39 +0100

    add security controller (migration error active)

---
+++ EmployeeManager/Controllers/SecurityController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManager.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManager.Controllers
+{
+    public class SecurityController : Controller
+    {
+        private readonly UserManager<AppIdentityUser> userManager;
+        private readonly SignInManager<AppIdentityUser> signInManager;
+        private readonly RoleManager<AppIdentityRole> roleManager;
+
+        public SecurityController(UserManager<AppIdentityUser> userManager,
+            SignInManager<AppIdentityUser> signInManager,
+            RoleManager<AppIdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
+    }
+}
