8b37081bd3d22816c94971d1b5ac87a180802003
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-15 01:15:49 +0100

    add register action (migration error active)

--- EmployeeManager/Controllers/SecurityController.cs
+++ EmployeeManager/Controllers/SecurityController.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManager.Security;
+﻿using EmployeeManager.Models;
+using EmployeeManager.Security;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,45 @@ namespace EmployeeManager.Controllers
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!roleManager.RoleExistsAsync("Manager").Result)
+                {
+                    var role = new AppIdentityRole();
+                    role.Name = "Manager";
+                    role.Description = "Can Perform CRUD Operations";
+                    var roleResult = roleManager.CreateAsync(role).Result;
+                }
+
+                var user = new AppIdentityUser();
+                user.UserName = register.UserName;
+                user.Email = register.Email;
+                user.FullName = register.FullName;
+                user.BirthDate = register.BirthDate;
+
+                var result = userManager.CreateAsync(user, register.Password).Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Manager").Wait();
+                    return RedirectToAction("SignIn", "Security");
+                } else
+                {
+                    ModelState.AddModelError("", "Invalid User Detail");
+                }
+            }
+            return View(register);
         }
     }
 }
