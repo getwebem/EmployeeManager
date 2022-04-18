fd1678fdf6baffd7b647234fb1b2b1d7ee60746f
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-18 09:01:23 +0100

    add security folder and update programe.cs

--- EmployeeManager.RazorPages/Program.cs
+++ EmployeeManager.RazorPages/Program.cs
@@ -1,21 +1,38 @@
+using EmployeeManager.Models;
+using EmployeeManager.Security;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+var DbConnect = "Server=(localdb)\\mssqllocaldb;Database=Northwind;integrated security=true";
+
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(DbConnect));
+builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(DbConnect));
+builder.Services.AddIdentity<AppIdentityUser, AppIdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Security/SignIn";
+    options.AccessDeniedPath = "/Security/AccessDenied";
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=EmployeeManager}/{action=List}/{id?}");
 
 app.Run();
