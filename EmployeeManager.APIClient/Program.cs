7c762442e977d94e195d8d7e703e60afd4e39496
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-20 23:34:15 +0100

    added http client

--- EmployeeManager.APIClient/Program.cs
+++ EmployeeManager.APIClient/Program.cs
@@ -1,11 +1,16 @@
 using EmployeeManager.APIClient.Security;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var DbConnect = "Server=(localdb)\\mssqllocaldb;Database=Northwind;integrated security=true";
+
+var BaseUrl = "http://localhost:17003";
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +22,11 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.LoginPath = "/Security/SignIn";
     options.AccessDeniedPath = "/Security/AccessDenied";
 });
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri(BaseUrl);
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+builder.Services.AddSingleton<HttpClient>(client);
 
 
 var app = builder.Build();
