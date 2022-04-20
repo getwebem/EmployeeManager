6a583fdae4f28ab07a7cb3e3a0dbb029711561e3
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-04-20 07:49:18 +0100

    add employee interface

---
+++ EmployeeManager.API/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManager.API.Models;
+
+namespace EmployeeManager.API.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        List<Employee> SelectAll();
+        Employee SelectByID(int id);
+        void Insert(Employee emp);
+        void Update(Employee emp);
+        void Delete(int id);
+    }
+}
