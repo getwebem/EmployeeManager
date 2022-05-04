fd220329dfefda7191c3fc50d1049e1b77e7f978
Kelvin Mbewe <kelvinmbewe@microsoft.com>
2022-05-04 12:31:15 +0100

    add interface and models folders

---
+++ EmployeeManager.Blazor/Models/SignIn.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+//Added  Entity Framework 6.0.4 migration 
+// (1) Add-Migration -Context AppDbContext
+// (2) update-database -Context AppIdentityDbContext
+//https://www.youtube.com/watch?v=BfEjDD8mWYg
+
+namespace EmployeeManager.Models
+{
+    public class SignIn
+    {
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+    }
+}
---
+++ EmployeeManager.Blazor/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManager.Blazor.Repositories
+{
+    public class IEmployeeRepository
+    {
+        List<Employee> SelectAll();
+        Employee SelectByID(int id);
+        void Insert(Employee emp);
+        void Update(Employee emp);
+        void Delete(int id);
+        List<string> SelectCountries();
+    }
+}
